f5c867735e1a2481a0a759efb8517fed95687317
VishnuThobbi <59354450+VishnuThobbi@users.noreply.github.com>
2020-09-15 16:58:57 +0530

    CAR

---
+++ JavaInit/Car.java
@@ -0,0 +1,16 @@
+
+import java.util.List;
+
+public class Car {
+
+	public Car(List<Wheel> wheels, int price, Engine1 engine) {
+		super();
+		this.wheels = wheels;
+		this.price = price;
+		this.engine = engine;
+	}
+	List<Wheel> wheels;
+	int price;
+	public Engine1 engine;
+	}
+
