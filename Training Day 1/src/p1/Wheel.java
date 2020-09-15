cb967522c7d86991b2225404224b14fad8348b7e
Boguspushkar <ladkat.pushkar@gmail.com>
2020-09-15 17:27:32 +0530

    git commit

---
+++ Training Day 1/src/p1/Wheel.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class Wheel {
+	
+	public Wheel(float thickness, float weight) {
+		super();
+		this.thickness = thickness;
+		this.weight = weight;
+	}
+	private float weight;
+	private float thickness;
+
+}
