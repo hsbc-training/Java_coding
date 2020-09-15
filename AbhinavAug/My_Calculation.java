a6e64c7518b5cfc856ba34c8f98becc45dffc579
AbhinavAug <71271203+AbhinavAug@users.noreply.github.com>
2020-09-15 17:11:26 +0530

    Create My_Calculation.java

---
+++ AbhinavAug/My_Calculation.java
@@ -0,0 +1,11 @@
+
+
+	public class My_Calculation extends Calculation {
+		   public void multiplication(int x, int y) {
+		      z = x * y;
+		      System.out.println("The product of the given numbers:"+z);
+		   }
+			
+
+}
+
