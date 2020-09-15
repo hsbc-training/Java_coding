1623bb6db1ea66a91bb78bd730a2d4ec5f369661
AbhinavAug <71271203+AbhinavAug@users.noreply.github.com>
2020-09-15 17:11:11 +0530

    Create Calculationmain.java

---
+++ AbhinavAug/Calculationmain.java
@@ -0,0 +1,11 @@
+
+public class Calculationmain {
+	 public static void main(String args[]) {
+	      int a = 20, b = 10;
+	      My_Calculation demo = new My_Calculation();
+	      demo.addition(a, b);
+	      demo.Subtraction(a, b);
+	      demo.multiplication(a, b);
+	   }
+
+}
