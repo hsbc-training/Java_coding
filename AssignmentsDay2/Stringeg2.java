f495d5cccc4295959fd0eab8922209de3ec94a3a
sid1306 <36204548+sid1306@users.noreply.github.com>
2020-09-16 21:16:21 +0530

    AssginDay2

---
+++ AssignmentsDay2/Stringeg2.java
@@ -0,0 +1,9 @@
+package prac;
+
+public class Stringeg2 {
+	 public static void main(String args[]) {
+	      char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+	      String helloString = new String(helloArray);  
+	      System.out.println( helloString );
+	   }
+}
