24abd4b8fc007a04e3650113036e82497925c86f
VishnuThobbi <59354450+VishnuThobbi@users.noreply.github.com>
2020-09-16 10:14:37 +0530

    loop decision string arrays

---
+++ Wednesday/src/Array.java
@@ -0,0 +1,19 @@
+
+
+
+public class Array {
+	public static void main(String args[]){
+		
+		
+		double array[]={4,1,2,3,5};
+		
+		
+		float sum=0;
+		
+		for(double a : array){
+			sum+=a;
+		}
+		System.out.println(sum);
+		
+	}
+}
