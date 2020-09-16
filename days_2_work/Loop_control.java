90a3ef0db73ecd86600ed30726b6b1b1381d14cd
singh-ujjwal <singhujjwal1912@gmail.com>
2020-09-16 10:00:37 +0530

    session_2

---
+++ days_2_work/Loop_control.java
@@ -0,0 +1,17 @@
+package days_2_work;
+
+public class Loop_control {
+	public static void main(String[] args) {
+		int[] number = {10,20,30,40};
+		for(int x : number) {
+			System.out.println(x);
+		}
+		System.out.print("\n");
+	    String [] names = {"James", "Larry", "Tom", "Lacy"};
+
+	    for( String name : names ) {
+	       System.out.print( name );
+	    }
+	}
+	
+}
---
+++ days_2_work/StringDemo.java
@@ -0,0 +1,9 @@
+package days_2_work;
+
+public class StringDemo {
+	public static void main(String[] args) {
+		char[] helloArray = {'h','e','l','l','o'};
+		String helloString = new String(helloArray);
+		System.out.println(helloString);
+	}
+}
