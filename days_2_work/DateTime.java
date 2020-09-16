baf4f13e32cfea944c2c1e6a7a3c1b647297add3
singh-ujjwal <singhujjwal1912@gmail.com>
2020-09-16 10:14:14 +0530

    changes

---
+++ days_2_work/DateTime.java
@@ -0,0 +1,9 @@
+package days_2_work;
+import java.util.Date;
+
+public class DateTime {
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println(date.toString());
+	}
+}
