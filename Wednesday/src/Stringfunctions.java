d5ad35381ca19260d391ddd08e8c8356b5054fbf
VishnuThobbi <59354450+VishnuThobbi@users.noreply.github.com>
2020-09-16 10:00:48 +0530

    String Functions

---
+++ Wednesday/src/Stringfunctions.java
@@ -0,0 +1,21 @@
+
+public class Stringfunctions {
+	static String str = "Hong Kong and Shanghai";
+	static String str1 = " Banking Corp";
+			public static void main(String args[]){
+	
+	int l=str.length();
+	
+	System.out.println(str.concat(str1));
+	
+	System.out.println(str.compareTo(str1));
+	
+	System.out.println(str.charAt(2));
+	
+	System.out.println(str.indexOf('K'));
+	
+	
+	
+	
+	}
+}
