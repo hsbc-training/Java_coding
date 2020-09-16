2664a79330b98b83282dba43202fc65f34c9f6aa
ritakshi <ritakshi.sen@gmail.com>
2020-09-16 17:15:50 +0530

    Day2 codes

---
+++ Day2/src/Day2/StringDemo.java
@@ -0,0 +1,14 @@
+package Day2;
+
+import java.lang.String;
+
+public class StringDemo {
+public static void main(String[] args) {
+	String S1 = "HSBC";
+	String S2 = "HSBC";
+	System.out.println(S1.hashCode());
+	System.out.println(S2.hashCode());
+	System.out.println(S1 == S2);
+	
+}
+}
