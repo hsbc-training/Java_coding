b5957db13f868321ae522426108b5be68dec036b
VishnuThobbi <59354450+VishnuThobbi@users.noreply.github.com>
2020-09-16 10:20:10 +0530

    loop date array decision

---
+++ Wednesday/src/DateProgram.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+import java.text.*;
+
+public class DateProgram {
+	public static void main(String args[]){
+		Date date = new Date();
+		
+		System.out.println(date.toString());
+		
+		Date form = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat();
+	}
+}
