3121c7ffda540d667d8366feeb83250a6dc4d05c
VishnuThobbi <59354450+VishnuThobbi@users.noreply.github.com>
2020-09-17 16:10:01 +0530

    Threads

---
+++ Thread-demo/src/Worker.java
@@ -0,0 +1,11 @@
+
+public class Worker implements Runnable{
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("In Worker Thread"+Thread.currentThread());
+	}
+
+}
+
+	
