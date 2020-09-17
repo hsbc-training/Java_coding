2553babdb737ea173e2df6c9f86ba6781362f339
smritibagga <71281136+smritibagga@users.noreply.github.com>
2020-09-17 17:17:13 +0530

    Add files via upload

---
+++ Day3/ThreadsDemo/Thread_Demo.java
@@ -0,0 +1,11 @@
+package ThreadsDemo;
+
+public class Thread_Demo {
+public static void main(String[] args) {
+	System.out.println("code executed in which thread="+Thread.currentThread());
+	Thread t1=new Thread(new Worker());
+	t1.start();
+	Thread t2=new Thread(new Worker());
+	t2.start();
+}
+}
