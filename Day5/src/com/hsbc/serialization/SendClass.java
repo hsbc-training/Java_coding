1ea50cf1764fbd0d852e15e3c01abccf60c73f49
amishabhardwaj <amisha.lko@gmail.com>
2020-09-21 17:01:02 +0530

    Added day 5 content

--- Day5/src/com/hsbc/serialization/SendClass.java
+++ Day5/src/com/hsbc/serialization/SendClass.java
@@ -1,21 +1,21 @@
 package com.hsbc.serialization;
+
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
-public class SendClass
-{
-  public static void main(String args[])
-  {
-    Student obj = new Student(101, 25, "Chaitanya", "Agra", 6);
-    try{ 
-      FileOutputStream fos = new FileOutputStream("Student.ser"); 
-      ObjectOutputStream oos = new ObjectOutputStream(fos);
-      oos.writeObject(obj);
-      oos.close();
-      fos.close();
-      System.out.println("Serialzation Done!!");
-   }catch(IOException ioe){
-      System.out.println(ioe);
-    }
-  }
+
+public class SendClass {
+	public static void main(String args[]) {
+		Student obj = new Student(101, 25, "Amisha", "Lucknow", 6);
+		try {
+			FileOutputStream fos = new FileOutputStream("Student.ser");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(obj);
+			oos.close();
+			fos.close();
+			System.out.println("Serialzation Done!!");
+		} catch (IOException ioe) {
+			System.out.println(ioe);
+		}
+	}
 }
