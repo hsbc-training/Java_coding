56e2374181bab31144d6d58fab8351d6209068d8
fareeya <37800330+fareeya@users.noreply.github.com>
2020-09-21 17:19:17 +0530

    day5.3_work

---
+++ DeserializationEg.java
@@ -0,0 +1,35 @@
+package day5_ExceptionHandling;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+public class DeserializationEg {
+
+ public static void main(String args[])
+ {
+    Student o=null;
+    try{
+      FileInputStream fis = new FileInputStream("Student.ser");
+      ObjectInputStream ois = new ObjectInputStream(fis);
+      o = (Student)ois.readObject();
+      ois.close();
+      fis.close();
+    }
+    catch(IOException ioe)
+    {
+       ioe.printStackTrace();
+       return;
+    }catch(ClassNotFoundException cnfe)
+     {
+       System.out.println("Student Class is not found.");
+       cnfe.printStackTrace();
+       return;
+     }
+    System.out.println("Student Name:"+o.getStuName());
+    System.out.println("Student Age:"+o.getStuAge());
+    System.out.println("Student Roll No:"+o.getStuRollNum());
+    System.out.println("Student Address:"+o.getStuAddress());
+    //System.out.println("Student Height:"+o.getStuHeight());
+ }
+}
+
