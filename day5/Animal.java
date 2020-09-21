d877500b74fb84ca166afa521ce27be27fb76f9b
fareeya <37800330+fareeya@users.noreply.github.com>
2020-09-21 11:39:05 +0530

    day5_work

---
+++ day5/Animal.java
@@ -0,0 +1,6 @@
+package day5;
+public class Animal{
+	   public void sound(){
+	      System.out.println("Animal is making a sound");   
+	   }
+	}
---
+++ day5/Horse.java
@@ -0,0 +1,12 @@
+package day5;
+
+public class Horse extends Animal{
+    @Override
+    public void sound(){
+        System.out.println("Neigh");
+    }
+    public static void main(String args[]){
+    	Animal obj = new Horse();
+    	obj.sound();
+    }
+}
