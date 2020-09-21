af76d77d4c9514614e502b808f7d7b095062fd3c
singh-ujjwal <singhujjwal1912@gmail.com>
2020-09-21 16:59:46 +0530

    new changes

---
+++ Day_5/src/serilization_prac/SendClass.java
@@ -0,0 +1,23 @@
+package serilization_prac;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+
+public class SendClass
+{
+  public static void main(String args[])
+  {
+    Student obj = new Student(101, 25, "Chaitanya", "Agra", 6);
+    try{ 
+      FileOutputStream fos = new FileOutputStream("Student.ser"); 
+      ObjectOutputStream oos = new ObjectOutputStream(fos);
+      oos.writeObject(obj);
+      oos.close();
+      fos.close();
+      System.out.println("Serialzation Done!!");
+   }catch(IOException ioe){
+      System.out.println(ioe);
+    }
+  }
+}
