6d3e2331598e6ef70a17991e2a43f48313af3f25
VishnuThobbi <59354450+VishnuThobbi@users.noreply.github.com>
2020-09-21 09:39:07 +0530

    Day-5

--- Practice/src/EnumPractice/Booking.java
+++ Practice/src/EnumPractice/Booking.java
@@ -7,7 +7,7 @@ public class Booking {
 	public static void main(String[] args) {
 		
 		List<Car> car = new ArrayList<>();
-		car.add(new Car(COLOR.BLACK,200000));
+		car.add(new Car(COLOR.RED,200000));
 		
 		Car c = new Car(COLOR.RED,200000);
 		System.out.println(c.getPrice() + "" + c.getColor() + car);
